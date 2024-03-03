01661a5c200bcfcc812bc3e301edf8c7a46a3938
Jeff Capshaw <51003293+jcapshaw@users.noreply.github.com>
2024-03-03 01:27:29 +0000

    feat: Fix style issues  and MessageCard.css files and add CodeSandbox tasks and devcontainer configuration files

--- src/component/DefaultLayout/DefaultLayout.jsx
+++ src/component/DefaultLayout/DefaultLayout.jsx
@@ -1,40 +1,48 @@
-import { GithubOutlined } from '@ant-design/icons';
+import { GithubOutlined } from "@ant-design/icons";
 import { useHistory } from "react-router-dom";
-import './DefaultLayout.css'
+import "./DefaultLayout.css";
 
-const DefaultLayout = ({children}) => {
-  const history = useHistory()
+const DefaultLayout = ({ children }) => {
+  const history = useHistory();
 
   const navigateToAuthenticationPage = () => {
-    history.push('/authentication')
-  }
+    history.push("/authentication");
+  };
   const navigateToPhoneNumbersPage = () => {
-    history.push('/phone-numbers')
-  }
+    history.push("/phone-numbers");
+  };
 
   return (
     <>
       <div className="default-layout-container container grid-lg">
-        <header className="navbar bg-primary" style={{padding: '.5em'}}>
+        <header className="navbar bg-primary" style={{ padding: ".5em" }}>
           <section className="navbar-section">
-            <span className="btn btn-link text-light" onClick={navigateToAuthenticationPage}>Authentication</span>
-            <span className="btn btn-link text-light" onClick={navigateToPhoneNumbersPage}>Messages</span>
+            <span
+              className="btn btn-link text-light"
+              onClick={navigateToAuthenticationPage}
+            >
+              Authentication
+            </span>
+            <span
+              className="btn btn-link text-light"
+              onClick={navigateToPhoneNumbersPage}
+            >
+              Messages
+            </span>
           </section>
           <section className="navbar-center hide-xs">
-            <span className="h5">Twilio SMS Web</span>
-          </section>
-          <section className="navbar-section">
-            <a className="btn btn-link text-light" href="https://github.com/rafasantos/twilio-sms-web" target="_blank" rel="noreferrer">
-              GitHub <GithubOutlined/>
-            </a>
+            <span className="h5">Dealer Acumen SMS Tool</span>
           </section>
         </header>
       </div>
-      <div className="default-layout-container container grid-lg text-left" style={{marginTop: '1em'}}>
+      <div
+        className="default-layout-container container grid-lg text-left"
+        style={{ marginTop: "1em" }}
+      >
         {children}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default DefaultLayout
+export default DefaultLayout;
